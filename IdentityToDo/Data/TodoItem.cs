810ed472840d57fe9e111ae221ae80a7b33f7938
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-03 15:28:23 +0300

    first commit

---
+++ IdentityToDo/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace IdentityToDo.Data;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Task { get; set; }
+    public bool Complete { get; set; }
+    public string? Owner { get; set; }
+}
